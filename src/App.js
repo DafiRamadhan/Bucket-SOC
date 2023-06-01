5d7c0a116fdb8b76b3dc2951f01f473b54f22a19
DafiRamadhan <mdafiramadhan@yahoo.com>
2023-06-01 15:07:12 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -22,14 +22,50 @@ function App() {
     getData('user').then(res => {
       //jika datanya ada
       if (res) {
-        setStatus('Logged In');
-        SplashScreen.hide();
+        //Cek data akun di database
+        return onValue(
+          ref(getDatabase(), '/users/' + res.uid),
+          snapshot => {
+            const data = snapshot.val();
+            //jika akun ditemukan di database
+            if (data) {
+              //Cek status login di firebase Auth
+              getAuth().onAuthStateChanged(function (user) {
+                if (user) {
+                  // User is signed in.
+                  setStatus('Logged In');
+                  SplashScreen.hide();
+                } else {
+                  // No user is signed in.
+                  setStatus('Not Login');
+                  clearData();
+                  SplashScreen.hide();
+                }
+              });
+              //Jika akun tidak ditemukan di database
+            } else {
+              setStatus('Not Login');
+              clearData();
+              SplashScreen.hide();
+            }
+          },
+          {
+            onlyOnce: true,
+          },
+          error => {
+            //ERROR baca data firebase
+            setStatus('Not Login');
+            clearData();
+            SplashScreen.hide();
+          },
+        );
+        //Jika tidak ada data user di localStorage
       } else {
         setStatus('Not Login');
         SplashScreen.hide();
       }
     });
-  })
+  });
   
   const [status, setStatus] = useState(false);
   return (
